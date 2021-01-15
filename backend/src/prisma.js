43afe26307f1f84ea5e0ce542924bb6f1a89b974
yinon <yi7non@gmail.com>
2021-01-15 06:11:53 +0200

    try to add docker container

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,9 +1,9 @@
-// ddk475g7pg07e6 - database name 
+// ddk475g7pg07e6 - database name
 const { Prisma } = require('prisma-binding')
 
 const prisma = new Prisma({
-    typeDefs: "src/generated/prisma.graphql",
-    endpoint: "http://localhost:4466/"
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466/'
 })
 
 module.exports = prisma
